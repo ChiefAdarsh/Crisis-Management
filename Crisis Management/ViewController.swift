c3b451a38c5aa4926e7285a99ec4e1666ed0316a
Adarsh Goura <adarshgoura5@gmail.com>
2022-11-03 22:44:54 -0500

    Home Screen

--- Crisis Management/ViewController.swift
+++ Crisis Management/ViewController.swift
@@ -9,16 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var mainLabel: UILabel!
-    @IBOutlet var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func buttonPressed(_ sender: Any) {
-        mainLabel.text = "This app doesn't rock!"
-    }
 
 
 }
