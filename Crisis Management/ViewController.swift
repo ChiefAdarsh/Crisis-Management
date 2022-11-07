347fbed0bb9adc23a5818af7fbb8a47ec84cf3f4
= <91765010+ariIndu@users.noreply.github.com>
2022-11-07 13:48:50 -0600

    Fixed home screen

--- Crisis Management/ViewController.swift
+++ Crisis Management/ViewController.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Call911Btn: UIButton!
+    @IBOutlet weak var ReportBtn: UIButton!
+    @IBOutlet weak var ProtocolsBtn: UIButton!
+    @IBOutlet weak var CallAdminBtn: UIButton!
+    @IBOutlet weak var ResourcesBtn: UIButton!
+    @IBOutlet var HomeBtns: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        roundBtns(radius: 15)
         // Do any additional setup after loading the view.
     }
+    
+    func roundBtns(radius: Double) {
+        for btn in HomeBtns {
+            btn.layer.cornerRadius = CGFloat(radius)
+            btn.layer.masksToBounds = true
+        }
+    }
 
-
-
+    @IBAction func Call911(_ sender: Any) {
+//        let alertController = UIAlertController(title: "Alert", message: "Are you sure you want to call 911", preferredStyle: .alert)
+//        alertController.addAction(UIAlertAction(title: "No", style: .default))
+//        alertController.addAction(UIAlertAction(title: "Call", style: .destructive, handler: { (_) in
+//            print("911")
+//        }))
+        print("911")
+    }
+    
 }
-
