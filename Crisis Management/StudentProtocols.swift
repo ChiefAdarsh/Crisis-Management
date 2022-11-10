e1c267de4ce971e32c55c7e17aa640d6cc7c9574
Adarsh Goura <adarshgoura5@gmail.com>
2022-11-10 14:03:45 -0600

    Fixed Student Protocols 2:00pm 11/10

--- Crisis Management/StudentProtocols.swift
+++ Crisis Management/StudentProtocols.swift
@@ -91,29 +91,19 @@ class ChecklistViewController: UIViewController {
     
 }
 
-class ProtocolsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ProtocolsViewController: UIViewController {
     
-    @IBOutlet var pickerView: UIPickerView!
     
-    let cities: [String] = ["Lewisville", "Denton","Plano", "Coppell", "Dallas", "Fort Worth", "Frisco", "Carollton", "Farmers Branch", "Richardson", "Colleyville", "Addison", "Houston", "Canton", "Lemont", "Judsonia"]
-    
-
-    @IBOutlet var infoTextView: UITextView!
-    @IBOutlet var subtitle: UILabel!
     var inOrOut: String!
-    var placementAnswer = 0
+    
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        // Do any additional setup after loading the view.
-        
-        subtitle.text = InOutSchoolController.insideSchool ? "In School" : "Out of School"
     }
     //Goes to Call Recommended people screen
+
     @IBAction func CallRecommended(_ sender: Any) {
         var callAdminController: UIViewController!
         if InOutSchoolController.insideSchool {
@@ -123,46 +113,7 @@ class ProtocolsViewController: UIViewController, UIPickerViewDelegate, UIPickerV
         }
         self.navigationController?.pushViewController(callAdminController, animated: true)
     }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        placementAnswer = row
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return cities.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return cities[row]
-    }
-    
-    @IBAction func Enter(_ sender: Any) {
-        var infoString = ""
-        var selectedList: [Resource] = []
-        for resource in Resource.myArray {
-            if resource.city == cities[placementAnswer] {
-                selectedList.append(resource)
-            }
-        }
-        
-        infoString.append("Resources in \(cities[placementAnswer])\n")
-        for resource in selectedList {
-            infoString.append(resource.category + "\n")
-            infoString.append(resource.name + "\n")
-            infoString.append(resource.contact + "\n")
-            infoString.append(resource.address + "\n")
-            infoString.append(resource.city + "\n")
-            infoString.append(resource.state + "\n")
-            infoString.append(resource.zip + "\n")
-            infoString.append(resource.website + "\n\n")
-//            infoString.append(resource.addInfo + "\n\n")
-        }
-        
-        infoTextView.text = infoString
-    }
 }
+
 
 //  infolabel.text =
