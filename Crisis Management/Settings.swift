d27cdea21731a3a32217a3e3276c5ab8bd8d4875
Adarsh Goura <adarshgoura5@gmail.com>
2022-11-10 21:37:54 -0600

    9:40 Nov 10 Feedback

--- Crisis Management/Settings.swift
+++ Crisis Management/Settings.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class SettingsController: UITableViewController {
+    var myIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // ...
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        myIndex = indexPath.row
+        
+        if (myIndex == 4){
+            guard let url = URL(string: "https://forms.gle/XVt2PCnT6WnN1ngb8") else {
+                return
+            }
+            UIApplication.shared.open(url)
+        }
+        
+        
+        //performSegue(withIdentifier: "resIden", sender: self)
+    }
+    
 }
 
 class PrivacyNoticeController: UIViewController {
