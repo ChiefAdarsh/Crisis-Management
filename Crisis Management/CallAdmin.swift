20ab7a5385aa35094956db7edede858c4ea7a17d
= <91765010+ariIndu@users.noreply.github.com>
2022-11-11 00:18:03 -0600

    In school contact admin fixed; more efficient

--- Crisis Management/CallAdmin.swift
+++ Crisis Management/CallAdmin.swift
@@ -9,6 +9,138 @@ import UIKit
 
 // MARK: - IN SCHOOL
 
+var numberStart = "214-496-"
+
+var adminList: [Admin] = []
+var principalList: [Admin] = []
+var counselorList: [Admin] = []
+
+var selectedAdmin: Admin = Admin(imgStr: "", adminType: AdminType.Counselor, lastName: "", firstName: "", username: "", callExt: 0, adminTypeDetailed: "")
+
+enum AdminType {
+    case Principal
+    case Counselor
+}
+
+class Admin {
+    
+    var adminType: AdminType
+    var adminTypeDetailed: String
+    var lastName: String
+    var firstName: String
+    var username: String
+    var callExt: Int
+    var imgStr: String
+    
+    var fullName: String {
+        return "\(firstName) \(lastName)"
+    }
+    
+    var number: String {
+        return numberStart + String(callExt)
+    }
+    
+    var email: String {
+        return username + "@coppellisd.com"
+    }
+    
+    var callLink: String {
+        return "https://teams.microsoft.com/l/chat/0/0?users=\(email)"
+    }
+    
+    var cellString: String {
+        return "\(fullName)\t-\t\(callExt)"
+    }
+    
+    init(imgStr: String, adminType: AdminType, lastName: String, firstName: String, username: String, callExt: Int, adminTypeDetailed: String) {
+        self.adminType = adminType
+        self.lastName = lastName
+        self.firstName = firstName
+        self.username = username
+        self.callExt = callExt
+        self.adminTypeDetailed = adminTypeDetailed
+        self.imgStr = imgStr
+    }
+    
+}
+
+func createAdmins() {
+    
+    adminList.removeAll()
+    principalList.removeAll()
+    counselorList.removeAll()
+    
+    // PRINCIPALS
+
+    var admin = Admin(imgStr: "Springer_crop", adminType: AdminType.Principal, lastName: "Springer", firstName: "Laura", username: "lspringer", callExt: 6104, adminTypeDetailed: "Principal")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Arnold_crop", adminType: AdminType.Principal, lastName: "Arnold", firstName: "Melissa", username: "marnold", callExt: 6122, adminTypeDetailed: "Associate Principal")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Rix_crop", adminType: AdminType.Principal, lastName: "Rix", firstName: "Aylor", username: "arix", callExt: 6124, adminTypeDetailed: "Assistant Principal (A-C)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Osborne_crop", adminType: AdminType.Principal, lastName: "Osborne", firstName: "Cindi", username: "cosborne", callExt: 6126, adminTypeDetailed: "Assistant Principal (D-J)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Gollner_crop", adminType: AdminType.Principal, lastName: "Gollner", firstName: "Chris", username: "cgollner", callExt: 6123, adminTypeDetailed: "Assistant Principal (K-M)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Porter_crop", adminType: AdminType.Principal, lastName: "Porter", firstName: "Zane", username: "zporter", callExt: 0, adminTypeDetailed: "Assistant Principal (N-Sf)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Girard_crop", adminType: AdminType.Principal, lastName: "Girard", firstName: "Brandon", username: "bgirard", callExt: 0, adminTypeDetailed: "Assistant Principal (Sg-Z)")
+    adminList.append(admin)
+
+    // COUNSELORS
+
+    admin = Admin(imgStr: "Cinelli_crop", adminType: AdminType.Counselor, lastName: "Cinelli", firstName: "Ann", username: "acinelli", callExt: 6112, adminTypeDetailed: "Lead Counselor (Rod-Som)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Abreu_crop", adminType: AdminType.Counselor, lastName: "Abreu", firstName: "Cheryl", username: "cabreu", callExt: 6132, adminTypeDetailed: "Counselor (A-Cham)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "McMillin_crop", adminType: AdminType.Counselor, lastName: "McMillin", firstName: "Laura", username: "lmcmillin", callExt: 6106, adminTypeDetailed: "Counselor (Chan-Gox)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Kennington_crop", adminType: AdminType.Counselor, lastName: "Kennington", firstName: "Michael", username: "mkennington", callExt: 6133, adminTypeDetailed: "Counselor (Goy-Kiv)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Oh_crop", adminType: AdminType.Counselor, lastName: "Oh", firstName: "Lindsey", username: "loh", callExt: 6136, adminTypeDetailed: "Counselor (Nal-Roc)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Tremethick_crop", adminType: AdminType.Counselor, lastName: "Tremethick", firstName: "Keith", username: "ktremethick", callExt: 6131, adminTypeDetailed: "Counselor (Son-Z)")
+    adminList.append(admin)
+
+    admin = Admin(imgStr: "Crumpton_crop", adminType: AdminType.Counselor, lastName: "Crumpton", firstName: "Heather", username: "hcrumpton", callExt: 6135, adminTypeDetailed: "Counselor (Kiw-Nah)")
+    adminList.append(admin)
+
+    // Sort
+    
+    for admin in adminList {
+        if admin.adminType == AdminType.Principal {
+            print("Principal \(admin.fullName)")
+            principalList.append(admin)
+        } else if admin.adminType == AdminType.Counselor {
+            print("Counselor \(admin.fullName)")
+            counselorList.append(admin)
+        } else {
+            print(admin.fullName)
+        }
+    }
+
+}
+
+func findAdmin(username: String) -> Admin {
+    for admin in adminList {
+        if admin.username == username {
+            return admin
+        }
+    }
+    return Admin(imgStr: "AA", adminType: AdminType.Counselor, lastName: "NA", firstName: "NA", username: "NA", callExt: 0, adminTypeDetailed: "NA")
+}
+
 class CallAdminInSchoolController: UIViewController {
 
     override func viewDidLoad() {
@@ -37,15 +169,135 @@ class CallAdminInSchoolController: UIViewController {
 }
 
 class PrincipalContactsTableViewController: UITableViewController {
+    
     override func viewDidLoad() {
+        createAdmins()
         super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return principalList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PrincipalCell", for: indexPath)
+
+        // Configure the cell...
+
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        for i in 0...(principalList.count - 1) {
+            if indexPath.row == i {
+                label.text = principalList[i].cellString
+                cell.accessoryType = .disclosureIndicator
+            }
+        }
+        // End of new code block
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView,didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+        
+        for i in 0...(principalList.count - 1) {
+            if indexPath[1] == i {
+                selectedAdmin = principalList[i]
+                print(selectedAdmin.fullName)
+                var prinViewController = storyboard!.instantiateViewController(withIdentifier: "adminInfoInSchool") as! AdminInfoViewController
+                self.navigationController?.pushViewController(prinViewController, animated: true)
+            }
+        }
     }
 }
 
 class CounselorContactsTableViewController: UITableViewController {
+    
     override func viewDidLoad() {
+        createAdmins()
         super.viewDidLoad()
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return counselorList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CounselorCell", for: indexPath)
+
+        // Configure the cell...
+
+        let label = cell.viewWithTag(2000) as! UILabel
+        
+        for i in 0...(principalList.count - 1) {
+            if indexPath.row == i {
+                label.text = counselorList[i].cellString
+                cell.accessoryType = .disclosureIndicator
+            }
+        }
+        // End of new code block
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView,didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+        
+        for i in 0...(counselorList.count - 1) {
+            if indexPath[1] == i {
+                selectedAdmin = counselorList[i]
+                print(selectedAdmin.fullName)
+                var counViewController = storyboard!.instantiateViewController(withIdentifier: "adminInfoInSchool") as! AdminInfoViewController
+                self.navigationController?.pushViewController(counViewController, animated: true)
+            }
+        }
+    }
+}
+
+class AdminInfoViewController: UIViewController {
+    
+    
+    
+    @IBOutlet var EmailBtn: UIButton!
+    @IBOutlet var CallBtn: UIButton!
+    @IBOutlet var AdminLbl: UILabel!
+    @IBOutlet var AdminPic: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        AdminLbl.text = "\(selectedAdmin.fullName), \(selectedAdmin.adminTypeDetailed)"
+        AdminPic.image = UIImage(named: selectedAdmin.imgStr)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func call(_ sender: Any) {
+        if let url = NSURL(string: selectedAdmin.callLink){
+            UIApplication.shared.open(url as URL)
+        }
+    }
+    
+    @IBAction func email(_ sender: Any) {
+        let email = selectedAdmin.email
+        if let url = URL(string: "mailto:\(email)") {
+          if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url)
+          } else {
+            UIApplication.shared.openURL(url)
+          }
+        }
+    }
+    
 }
 
 // MARK: - OUT OF SCHOOL
